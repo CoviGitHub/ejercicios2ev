0af3d4f334d256ebd4b998ca20db23e2a87483c9
CoviGitHub <queipo_6@hotmail.com>
2020-02-11 08:32:33 +0100

    Ejercicio 12 de la unidad 4 resuelto.

--- src/unidad4/Vectores7.java
+++ src/unidad4/Vectores7.java
@@ -3,28 +3,27 @@ package unidad4;
 public class Vectores7 {
 
 	public static void main(String[] args) {
-		int[] v = { 6, 2, 3, 0, 1 };
+//		int[] v = { 6, 2, 3, 0, 1 };
+		int[] v = { 1, 0, 3, 2, 6 };
 
-		String result = centro(v);
+		Integer result = centro(v);
 		System.out.print((result != null) ? "El centro es el índice: " + result : "No tiene centro");
 	}
 
-	public static String centro(int[] vect) {
-		double a = 0;
-		double b = 0;
-		int c = 1;
-		String centro = "";
-		for (int i = 0; i < c; i++) {
-			a += (c - i) * vect[i];
+	public static Integer centro(int[] vect) {
+		for (int c=1; c<vect.length-1; c++) {
+			double a = 0;
+			double b = 0;
+			
+			for (int i = 0; i < c; i++) {
+				a += (c - i) * vect[i];
+			}
+			for (int j = c + 1; j < vect.length; j++) {
+				b += (j - c) * vect[j];
+			}
+			if (a == b)
+				return c;
 		}
-		for (int j = c + 1; j < vect.length; j++) {
-			b += (j - c) * vect[j];
-
-		}
-		if (a == b && a != 0) {
-			centro = "" + (int) (a / b);
-			return centro;
-		} else
-			return null;
+		return null;
 	}
 }
