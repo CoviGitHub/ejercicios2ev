b9cbd57129276a5e9c7a530d9bd9da1c073900ed
CoviGitHub <queipo_6@hotmail.com>
2020-03-05 18:15:24 +0100

    Ejercicio 3 de la unidad 5 resuelto.

---
+++ src/unidad5/PruebaAutor.java
@@ -0,0 +1,11 @@
+package unidad5;
+
+public class PruebaAutor {
+
+	public static void main(String[] args) {
+		
+		Autor autor=new Autor("Pepito Pérez","pperez@gmail.com",Genero.MASCULINO);
+		System.out.println(autor);
+	}
+
+}
